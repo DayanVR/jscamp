0477ecdf83de42be718111ef8108e6fe2b7f17f1
DayanVR <dayanvillavicencio13@gmail.com>
2025-11-24 09:36:51 -0500

    refactor: replace NavLink with custom Link component using useRouter hook

--- 02-react/src/components/Link.jsx
+++ 02-react/src/components/Link.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { Link as NavLink } from "react-router";
-
-export default function Link({ href, children, ...restOfProps }) {
-  return (
-    <NavLink to={href} {...restOfProps}>
-      {children}
-    </NavLink>
-  );
-}
-=======
 import { useRouter } from "../hooks/useRouter"
 
 export function Link ({ href, children, ...restOfProps }) {
@@ -25,4 +14,3 @@ export function Link ({ href, children, ...restOfProps }) {
     </a>
   )
 }
->>>>>>> e5376c41753983c9b100c8fe96bf51de4c5083a6
